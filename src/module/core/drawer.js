7e7107e0628356fde1566728104089e2635d9128
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-03-31 04:02:49 +0700

    feat!!!: new method

--- src/module/core/drawer.js
+++ src/module/core/drawer.js
@@ -1,164 +1,108 @@
-"use strict";
+"use strict"
 
-import Garis from "../models/garis.js";
-import Polygon from "../models/polygon.js";
-import Rectangle from "../models/persegiPanjang.js";
-import createFragmentShader from "../shader/fragmentShader.js";
-import createVertexShader from "../shader/vertexShader.js";
+// import Garis from "../models/garis3.js"
+import GLSLProgram from "../utils/GLSLProgram.js"
+import GLDrawing from "./GLDrawing.js"
 
-import GLDrawing from "../models/GLDrawing.js";
-import Shape from "../models/shape.js";
+import ShapeTypes from "../type/shapeTypes.js"
 
-function createProgram(gl, vertexShader, fragmentShader) {
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
-    if (success) {
-        return program;
-    }
+import Shape from "../models/shape.js"
+import Garis from "../models/garis.js"
+import Polygon from "../models/polygon.js"
 
-    console.log(gl.getProgramInfoLog(program));
-    gl.deleteProgram(program);
-}
 
+/**
+ * @class Drawer2
+ * @description Drawer class for list of shapes
+ */
 class Drawer {
-    #program;
-    #gl;
-    #resolutionUniformLocation;
-    models;
-    modelCandidate;
+    /**
+     * @type {GLDrawing}
+     */
+    glDrawing;
 
-    constructor(){
-        let canvas = document.querySelector("#glcanvas")
-        this.#gl = canvas.getContext("webgl", { preserveDrawingBuffer: true });
+    /**
+     * @type {Shape[]}
+     * @description List of shapes
+     * @default []
+     */
+    Shapes;
 
-        if (!this.#gl) {
-            console.log("WebGL not supported");
-            return;
-        }
+    constructor() {
+        const [canvas, gl] = GLSLProgram.getCanvasAndGLFromidCanvas("glcanvas");
+        const program = GLSLProgram.createProgram(gl);
+        this.glDrawing = new GLDrawing(gl, program);
 
-        let vertexShader = createVertexShader(this.#gl);
-        let fragmentShader =createFragmentShader(this.#gl);
-
-        // setup GLSL program
-        this.#program = createProgram(this.#gl, vertexShader, fragmentShader);
-
-        // Setup Canvas
         var leftPanelWidth = (document.querySelector('.left-panel')).offsetWidth;
         var rightPanelWidth = (document.querySelector('.right-panel')).offsetWidth;
-        // var headerHight = (document.querySelector("header h1")).offsetWidth;
-        
+
         console.log(`window: ${window.innerWidth}, ${window.innerHeight}`);
-        this.#gl.canvas.width = this.#gl.canvas.clientWidth - leftPanelWidth - rightPanelWidth;
-        this.#gl.canvas.height = this.#gl.canvas.clientHeight;
-        this.#gl.viewport(leftPanelWidth,0, this.#gl.canvas.width, this.#gl.canvas.height);        
-        this.#gl.clearColor(1,1,1,1);
-        this.#gl.clear(this.#gl.COLOR_BUFFER_BIT);
-        
-        
-        // use program : only once for one shader program
-        this.#gl.useProgram(this.#program);
+        gl.canvas.width = gl.canvas.clientWidth - leftPanelWidth - rightPanelWidth;
+        gl.canvas.height = gl.canvas.clientHeight;
+        gl.viewport(leftPanelWidth, 0, gl.canvas.width, gl.canvas.height);
 
-        // Setup the resolution
-        this.#resolutionUniformLocation = this.#gl.getUniformLocation(this.#program, "u_resolution");
-        this.#gl.uniform2f(this.#resolutionUniformLocation, this.#gl.canvas.width, this.#gl.canvas.height);
-        
-        console.log(`canvas width: ${this.#gl.canvas.width} height: ${this.#gl.canvas.height}`);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-        // var matrix = [
-        //     1, 0, 0,
-        //     0, 1, 0,
-        //     0, 0, 1
-        // ];
-        // var matrixUniformLocation = this.#gl.getUniformLocation(this.#program, "u_matrix");
-        // this.#gl.uniformMatrix3fv(matrixUniformLocation, false, matrix);
+        gl.clear(gl.COLOR_BUFFER_BIT);
 
+        gl.useProgram(program);
 
-        // Empty models and model Candidate
-        this.models = [];
-        this.modelCandidate = null;
+        var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+        gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+        var matrix = [
+            2 / gl.canvas.width, 0, 0,
+            0, -2 / gl.canvas.height, 0,
+            -1, 1, 1
+        ]
+        var matrixUniformLocation = gl.getUniformLocation(program, "u_matrix");
+        gl.uniformMatrix3fv(matrixUniformLocation, false, matrix);
+
+        this.Shapes = [];       
     }
 
-    addModel() {
-        this.models.push(this.modelCandidate);
-        this.modelCandidate = null;
-    }
-
-    drawModels() {
-        this.models.forEach((model) => {
-            model.draw();
-            console.log("draw model");
+    drawAllShapes() {
+        this.Shapes.forEach(shape => {
+            this.glDrawing.drawShape(shape);
         });
     }
 
-    createModelCandidate(modelType){
-        let model = null;
-        if (modelType == "Garis") {
-            model = new Garis(this.#gl, this.#program,3, 3);
-            model.setPositions = [
-                100, 100,
-                800, 0,
-                0, 0,
-            ];
-            model.setColors = [
-                1, 0, 0.5, 1,
-                1, 0, 0.5, 1,
-                1, 0, 0.5, 1,
-            ];
-            console.log("draw garis");
-        } else if (modelType == "Polygon") {
-            model = new Polygon(this.#gl, this.#program, 5, 5);
-            model.setPositions = [
-                200, 400,
-                200, 0,
-                0, 0,
-                100, 50,
-                30, 20
-            ];
-            model.setColors = [
-                1, 0, 0.5, 1,
-                0, 1, 0.5, 1,
-                1, 0, 0.5, 1,
-            ];
-            console.log("draw poligon");
-        } else if (modelType == "Rectangle") {
-            model = new Rectangle(this.#gl, this.#program);
-            var r1 = Math.random();
-            var b1 = Math.random();
-            var g1 = Math.random();
-            model.setPositions = [
-                0, 0,
-                400, 0,
-                400, 300,
-                0, 0,
-                400, 300,
-                0, 300
-            ];
-            model.setColors = [
-                r1, b1, g1, 1,
-                r1, b1, g1, 1,
-                r1, b1, g1, 1,
-                b1, r1, g1, 1,
-                b1, r1, g1, 1,
-                b1, r1, g1, 1
-            ];
-            console.log("draw rectangle");
+    drawOneShape(shape) {
+        this.#addShape(shape);
+        this.glDrawing.drawShape(shape);
+    }
+
+    #addShape(shape) {
+        this.Shapes.push(shape);
+    }
+
+    #removeShape(shape) {
+        this.Shapes = this.Shapes.filter(s => s !== shape);
+    }
+
+    clearShapes() {
+        this.Shapes = [];
+        this.glDrawing.clear();
+    }
+
+    startDrawShape(shapeType, mousePosition) {
+        let shape = null;
+        switch (shapeType) {
+            case ShapeTypes.GARIS:
+                shape = new Garis(0, "Garis1", mousePosition);
+                break;
+            case ShapeTypes.POLYGON:
+                shape = new Polygon()
+                break;
+            default:
+                console.error(`ShapeType ${shapeType} is not recognized`);
+                return;
         }
 
-        this.modelCandidate = model;
-
-        model.drawSetup();
-        console.log("model", model);
-        model.draw();
+        this.#addShape(shape);
+        this.glDrawing.drawShape(shape);
     }
-
-    drawModels() {
-        this.models.forEach((model) => {
-            model.draw();
-        });
-    }
+        
 }
 
 export default Drawer;
