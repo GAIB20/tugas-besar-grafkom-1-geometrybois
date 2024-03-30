9057cd3dd8754e4f7f7fd15f7ab01dd6b25f95fb
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-03-31 04:00:29 +0700

    feat: object type

---
+++ src/module/type/bufferType.js
@@ -0,0 +1,6 @@
+const BufferType = {
+    POSITION : 0,
+    COLOR : 1
+}
+
+export default BufferType;
---
+++ src/module/type/shapeTypes.js
@@ -0,0 +1,8 @@
+const ShapeTypes = {
+    LINES: 'lines',
+    RECTANGLE: 'rectangle',
+    SQUARE: 'square',
+    POLYGON: 'polygon'
+};
+
+export default ShapeTypes;
