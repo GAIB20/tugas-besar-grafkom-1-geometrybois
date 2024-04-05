fc90909f1c3146500263779ed6cfe8f070294756
akmaldika <13521070@mahasiswa.itb.ac.id>
2024-04-05 17:40:09 +0700

    fix: transformation

--- src/module/utils/transform2.js
+++ src/module/utils/transform2.js
@@ -101,17 +101,35 @@ class Transform2 {
      * @description Multiply two matrices
      */
     static multiply(m1, m2) {
-        const result = new Array(9).fill(0);
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                let sum = 0;
-                for (let k = 0; k < 3; k++) {
-                    sum += m1[i * 3 + k] * m2[k * 3 + j];
-                }
-                result[i * 3 + j] = sum;
-            }
-        }
-        return result;
+        var a00 = m1[0 * 3 + 0];
+        var a01 = m1[0 * 3 + 1];
+        var a02 = m1[0 * 3 + 2];
+        var a10 = m1[1 * 3 + 0];
+        var a11 = m1[1 * 3 + 1];
+        var a12 = m1[1 * 3 + 2];
+        var a20 = m1[2 * 3 + 0];
+        var a21 = m1[2 * 3 + 1];
+        var a22 = m1[2 * 3 + 2];
+        var b00 = m2[0 * 3 + 0];
+        var b01 = m2[0 * 3 + 1];
+        var b02 = m2[0 * 3 + 2];
+        var b10 = m2[1 * 3 + 0];
+        var b11 = m2[1 * 3 + 1];
+        var b12 = m2[1 * 3 + 2];
+        var b20 = m2[2 * 3 + 0];
+        var b21 = m2[2 * 3 + 1];
+        var b22 = m2[2 * 3 + 2];
+        return [
+        b00 * a00 + b01 * a10 + b02 * a20,
+        b00 * a01 + b01 * a11 + b02 * a21,
+        b00 * a02 + b01 * a12 + b02 * a22,
+        b10 * a00 + b11 * a10 + b12 * a20,
+        b10 * a01 + b11 * a11 + b12 * a21,
+        b10 * a02 + b11 * a12 + b12 * a22,
+        b20 * a00 + b21 * a10 + b22 * a20,
+        b20 * a01 + b21 * a11 + b22 * a21,
+        b20 * a02 + b21 * a12 + b22 * a22,
+        ];
     }
 
     static translate(matrix, tx, ty) {
@@ -130,6 +148,10 @@ class Transform2 {
         return Transform2.multiply(matrix, Transform2.shear(shx));
     }
 
+    static degreeToRadian(degree) {
+        return degree * Math.PI / 180;
+    }
+
     static general(
         width, height,
         tx, ty,
@@ -144,6 +166,7 @@ class Transform2 {
         matrix = Transform2.rotate(matrix, angleInRadians);
         matrix = Transform2.scale(matrix, sx, sy);
         matrix = Transform2.shearing(matrix, sh);
+        console.log(matrix);
         return matrix;
     }
 }
